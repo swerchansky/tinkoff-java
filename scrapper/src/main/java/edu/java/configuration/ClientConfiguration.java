df21b0dec09a70979c1a8e40d1532384f2dc98e3
Tarchevskii Alexey <swerch.a@gmail.com>
2024-02-19 14:44:45 +0300

    remove redundant request headers

--- scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
+++ scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
@@ -2,18 +2,14 @@ package edu.java.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ClientConfiguration {
-    private static final String CONTENT_TYPE = "Content-Type";
-
     @Bean
     public WebClient githubWebClient(ApplicationConfig applicationConfig) {
         return WebClient.builder()
             .baseUrl(applicationConfig.baseGithubUrl())
-            .defaultHeader(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
             .build();
     }
 
@@ -21,7 +17,6 @@ public class ClientConfiguration {
     public WebClient stackOverflowWebClient(ApplicationConfig applicationConfig) {
         return WebClient.builder()
             .baseUrl(applicationConfig.baseStackOverflowUrl())
-            .defaultHeader(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
             .build();
     }
 }
