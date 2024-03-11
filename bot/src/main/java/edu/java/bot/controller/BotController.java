ddee97760f6b57bc7a2ada270790761bb6542c0e
Tarchevskii Alexey <swerch.a@gmail.com>
2024-03-11 16:26:54 +0300

    improve controller docs readability

--- bot/src/main/java/edu/java/bot/controller/BotController.java
+++ bot/src/main/java/edu/java/bot/controller/BotController.java
@@ -7,34 +7,21 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 public interface BotController {
-    @Operation(
-            operationId = "updatesPost",
-            summary = "Отправить обновление",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Обновление обработано"),
-                    @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса", content = {
-                            @Content(
-                                    mediaType = APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = ApiErrorResponse.class)
-                            )
-                    })
-            }
-    )
-    @RequestMapping(
-            value = "/update",
-            method = POST,
-            consumes = APPLICATION_JSON_VALUE,
-            produces = APPLICATION_JSON_VALUE
-    )
+    @Operation(responses = {
+        @ApiResponse(responseCode = "200", description = "Обновление обработано"),
+        @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса", content = {
+            @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ApiErrorResponse.class))
+        })
+    })
+    @PostMapping(value = "/update")
     void update(
-            @RequestBody
-            @Parameter(name = "LinkUpdateRequest", required = true)
-            LinkUpdateRequest request
+        @RequestBody
+        @Parameter(name = "LinkUpdateRequest", required = true)
+        LinkUpdateRequest request
     );
 }
--- scrapper/src/main/java/edu/java/controller/ScrapperController.java
+++ scrapper/src/main/java/edu/java/controller/ScrapperController.java
@@ -6,182 +6,82 @@ import edu.java.controller.dto.LinkResponse;
 import edu.java.controller.dto.ListLinksResponse;
 import edu.java.controller.dto.RemoveLinkRequest;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 public interface ScrapperController {
-    @Operation(
-            operationId = "tgChatPost",
-            summary = "Зарегистрировать чат",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Чат зарегистрирован"),
-                    @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса", content = {
-                            @Content(
-                                    mediaType = APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = ApiErrorResponse.class)
-                            )
-                    })
-            }
-    )
-    @RequestMapping(
-            value = "/tg-chat/{id}",
-            method = POST,
-            produces = APPLICATION_JSON_VALUE
-    )
-    void addChat(
-            @PathVariable
-            @Parameter(name = "id", required = true)
-            long id
-    );
+    @Operation(responses = {
+        @ApiResponse(responseCode = "200", description = "Чат зарегистрирован"),
+        @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса", content = {
+            @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ApiErrorResponse.class))
+        })
+    })
+    @PostMapping(value = "/tg-chat/{id}")
+    void addChat(@PathVariable long id);
 
-    @Operation(
-            operationId = "tgChatDelete",
-            summary = "Удалить чат",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Чат успешно удален"),
-                    @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса", content = {
-                            @Content(
-                                    mediaType = APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = ApiErrorResponse.class)
-                            )
-                    }),
-                    @ApiResponse(responseCode = "404", description = "Чат не существует", content = {
-                            @Content(
-                                    mediaType = APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = ApiErrorResponse.class)
-                            )
-                    })
-            }
-    )
-    @RequestMapping(
-            value = "/tg-chat/{id}",
-            method = DELETE,
-            produces = APPLICATION_JSON_VALUE
-    )
-    void deleteChat(
-            @PathVariable
-            @Parameter(name = "id", required = true)
-            long id
-    );
+    @Operation(responses = {
+        @ApiResponse(responseCode = "200", description = "Чат успешно удален"),
+        @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса", content = {
+            @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ApiErrorResponse.class))
+        }),
+        @ApiResponse(responseCode = "404", description = "Чат не существует", content = {
+            @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ApiErrorResponse.class))
+        })
+    })
+    @DeleteMapping(value = "/tg-chat/{id}")
+    void deleteChat(@PathVariable long id);
 
-    @Operation(
-            operationId = "linksGet",
-            summary = "Получить все отслеживаемые ссылки",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "ССылки успешно получены", content = {
-                            @Content(
-                                    mediaType = APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = ListLinksResponse.class)
-                            )
-                    }),
-                    @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса", content = {
-                            @Content(
-                                    mediaType = APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = ApiErrorResponse.class)
-                            )
-                    })
-            }
-    )
-    @RequestMapping(
-            value = "/links",
-            method = GET,
-            produces = APPLICATION_JSON_VALUE
-    )
-    ListLinksResponse getLinks(
-            @RequestHeader(value = "Tg-Chat-Id")
-            @Parameter(name = "Tg-Chat-Id", required = true)
-            long id
-    );
+    @Operation(responses = {
+        @ApiResponse(responseCode = "200", description = "ССылки успешно получены", content = {
+            @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ListLinksResponse.class))
+        }),
+        @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса", content = {
+            @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ApiErrorResponse.class))
+        })
+    })
+    @GetMapping(value = "/links")
+    ListLinksResponse getLinks(@RequestHeader(value = "Tg-Chat-Id") long id);
 
-    @Operation(
-            operationId = "linksPost",
-            summary = "Добавить отслеживание ссылки",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Ссылка успешно добавлена", content = {
-                            @Content(
-                                    mediaType = APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = LinkResponse.class)
-                            )
-                    }),
-                    @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса", content = {
-                            @Content(
-                                    mediaType = APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = ApiErrorResponse.class)
-                            )
-                    })
-            }
-    )
-    @RequestMapping(
-            value = "/links",
-            method = POST,
-            produces = APPLICATION_JSON_VALUE,
-            consumes = APPLICATION_JSON_VALUE
-    )
+    @Operation(responses = {
+        @ApiResponse(responseCode = "200", description = "Ссылка успешно добавлена", content = {
+            @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = LinkResponse.class))
+        }),
+        @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса", content = {
+            @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ApiErrorResponse.class))
+        })
+    })
+    @PostMapping(value = "/links")
     LinkResponse addLink(
-            @RequestHeader(value = "Tg-Chat-Id")
-            @Parameter(name = "Tg-Chat-Id", required = true)
-            long id,
-            @RequestBody
-            @Parameter(required = true, content = {
-                    @Content(
-                            mediaType = APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = AddLinkRequest.class)
-                    )
-            })
-            AddLinkRequest link
+        @RequestHeader(value = "Tg-Chat-Id")
+        long id,
+        @RequestBody
+        AddLinkRequest link
     );
 
-    @Operation(
-            operationId = "linksDelete",
-            summary = "Убрать отслеживание ссылки",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Сылка успешно добавлена", content = {
-                            @Content(
-                                    mediaType = APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = LinkResponse.class)
-                            )
-                    }),
-                    @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса", content = {
-                            @Content(
-                                    mediaType = APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = ApiErrorResponse.class)
-                            )
-                    }),
-                    @ApiResponse(responseCode = "404", description = "Ссылка не найдена", content = {
-                            @Content(
-                                    mediaType = APPLICATION_JSON_VALUE,
-                                    schema = @Schema(implementation = ApiErrorResponse.class)
-                            )
-                    })
-            }
-    )
-    @RequestMapping(
-            value = "/links",
-            method = DELETE,
-            produces = APPLICATION_JSON_VALUE,
-            consumes = APPLICATION_JSON_VALUE
-    )
+    @Operation(responses = {
+        @ApiResponse(responseCode = "200", description = "Сылка успешно добавлена", content = {
+            @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = LinkResponse.class))
+        }),
+        @ApiResponse(responseCode = "400", description = "Некорректные параметры запроса", content = {
+            @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ApiErrorResponse.class))
+        }),
+        @ApiResponse(responseCode = "404", description = "Ссылка не найдена", content = {
+            @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ApiErrorResponse.class))
+        })
+    })
+    @DeleteMapping(value = "/links")
     LinkResponse deleteLink(
-            @RequestHeader(value = "Tg-Chat-Id")
-            @Parameter(name = "Tg-Chat-Id", required = true)
-            long id,
-            @RequestBody
-            @Parameter(required = true, content = {
-                    @Content(
-                            mediaType = APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = RemoveLinkRequest.class)
-                    )
-            })
-            RemoveLinkRequest link
+        @RequestHeader(value = "Tg-Chat-Id")
+        long id,
+        @RequestBody
+        RemoveLinkRequest link
     );
 }
